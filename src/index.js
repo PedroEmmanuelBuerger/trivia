1f88572dbd541dae9bc03fe94cff00ffc313dcab
pedro emmanuel <sombraios@hotmail.com>
2023-02-09 16:40:25 -0300

    main-group-6 arrumando linter

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
 import { Provider } from 'react-redux';
+import App from './App';
 import store from './redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={ store }>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    ,
   </Provider>,
 );
 //
