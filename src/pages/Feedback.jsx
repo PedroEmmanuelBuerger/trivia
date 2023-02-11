0e4c72364aaaa08bc5afa09139d4ecf71f26f421
PauloFonseca <pfonseca.arq@gmail.com>
2023-02-11 13:16:05 -0300

    Requesito 15

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Proptypes from 'prop-types';
 import Header from '../components/Header';
 
 class Feedback extends Component {
+  returnLogin = () => {
+    const { history } = this.props;
+    history.push('/');
+  };
+
   render() {
     return (
 
       <div>
         <Header />
         <h1 data-testid="feedback-text">Feedback</h1>
+        <button
+          type="button"
+          data-testid="btn-play-again"
+          onClick={ this.returnLogin }
+        >
+          Play Again
+        </button>
       </div>
     );
   }
 }
+
+Feedback.propTypes = {
+  history: Proptypes.shape({ push: Proptypes.func.isRequired }).isRequired,
+};
 
 const mapStateToProps = (state) => ({
   gravatar: state.player.gravatarEmail,
