309f6397b32db1bd452bf300f2d8591c2e555f6d
PauloFonseca <pfonseca.arq@gmail.com>
2023-02-11 12:56:57 -0300

    Requesito 12

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Header from '../components/Header';
 
-export default class Feedback extends Component {
+class Feedback extends Component {
   render() {
     return (
-      <div data-testid="feedback-text">Feedback</div>
+
+      <div>
+        <Header />
+        <h1 data-testid="feedback-text">Feedback</h1>
+      </div>
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  gravatar: state.player.gravatarEmail,
+  name: state.player.name,
+  score: state.player.score,
+});
+
+export default connect(mapStateToProps)(Feedback);
