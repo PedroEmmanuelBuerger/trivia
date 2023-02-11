4913353d5dbe20749af4fd43f28b15eab8498946
PauloFonseca <pfonseca.arq@gmail.com>
2023-02-11 13:45:07 -0300

    Requisito 18

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -3,10 +3,22 @@ import { connect } from 'react-redux';
 import Proptypes from 'prop-types';
 
 class Ranking extends Component {
+  returnLogin = () => {
+    const { history } = this.props;
+    history.push('/');
+  };
+
   render() {
     return (
       <div>
         <h1 data-testid="ranking-title">Ranking</h1>
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ this.returnLogin }
+        >
+          Play Again
+        </button>
       </div>
     );
   }
