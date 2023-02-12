f5b104570329eae0f4ebe7c8fff959f39858ba29
pedro emmanuel <sombraios@hotmail.com>
2023-02-12 12:53:31 -0300

    resolvendo req14

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -2,10 +2,30 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Proptypes from 'prop-types';
 import Header from '../components/Header';
+import { resetScoreAndAccumulated } from '../redux/actions/index';
 
 class Feedback extends Component {
+  state = {
+    assertions: 0,
+  };
+
+  componentDidMount() {
+    this.getAssertions();
+  }
+
+  getAssertions = () => {
+    const number = 3;
+    const { assertions } = this.props;
+    if (assertions >= number) {
+      this.setState({ assertions: 'Well Done!' });
+    } else {
+      this.setState({ assertions: 'Could be better...' });
+    }
+  };
+
   returnLogin = () => {
-    const { history } = this.props;
+    const { history, dispatch } = this.props;
+    dispatch(resetScoreAndAccumulated());
     history.push('/');
   };
 
@@ -15,11 +35,11 @@ class Feedback extends Component {
   };
 
   render() {
+    const { assertions } = this.state;
     return (
-
       <div>
         <Header />
-        <h1 data-testid="feedback-text">Feedback</h1>
+        <h1 data-testid="feedback-text">{ assertions }</h1>
         <button
           type="button"
           data-testid="btn-play-again"
@@ -42,12 +62,15 @@ class Feedback extends Component {
 
 Feedback.propTypes = {
   history: Proptypes.shape({ push: Proptypes.func.isRequired }).isRequired,
+  assertions: Proptypes.number.isRequired,
+  dispatch: Proptypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   gravatar: state.player.gravatarEmail,
   name: state.player.name,
   score: state.player.score,
+  assertions: state.player.assertions,
 });
 
 export default connect(mapStateToProps)(Feedback);
